b99495afd20aec6462acf535a357571bc3832d14
이명하 <qnwkaudgk@naver.com>
2024-12-16 23:39:56 +0900

    3일차

--- login.js
+++ login.js
@@ -8,10 +8,11 @@ const logoutButton = document.querySelector("#logout_btn");
 const main = document.querySelector("main");
 const userName = document.querySelector("#user_name");
 const userDetail = document.querySelector("#user_info");
+const loadingIndicator = document.querySelector("#loading");
 
 axios.defaults.withCredentials = true;
 
-// form의 기본 submit 값 초기화
+// 폼 제출 이벤트 초기화
 form.addEventListener("submit", (e) => e.preventDefault());
 
 // 로그인 함수
@@ -27,12 +28,12 @@ function logout() {
   return axios.delete("http://localhost:3000");
 }
 
-// 유저 정보를 받아오는 함수
+// 유저 정보 요청 함수
 function getUserInfo() {
   return axios.get("http://localhost:3000");
 }
 
-// 유저 정보를 렌더링 시키는 함수
+// 유저 정보 렌더링 함수
 function renderUserInfo(userInfo) {
   main.style.display = "block";
   form.style.display = "none";
@@ -40,7 +41,7 @@ function renderUserInfo(userInfo) {
   userDetail.textContent = userInfo.user_info;
 }
 
-// 유저 정보를 숨기는 함수
+// 로그인 폼 렌더링 함수
 function renderLoginForm() {
   main.style.display = "none";
   form.style.display = "block";
@@ -48,17 +49,35 @@ function renderLoginForm() {
   userDetail.textContent = "";
 }
 
-// 로그인 버튼을 클릭하는 경우 post 요청 보낸 후 get 요청
+// 로딩 상태 표시 함수
+function toggleLoading(isLoading) {
+  loadingIndicator.style.display = isLoading ? "block" : "none";
+}
+
+// 로그인 버튼 클릭 이벤트
 loginButton.onclick = () => {
+  toggleLoading(true);
   login()
-    .then(() => getUserInfo())
-    .then((res) => renderUserInfo(res.data));
+    .then(() => {
+      passwordInput.value = ""; // 비밀번호 초기화
+      return getUserInfo();
+    })
+    .then((res) => renderUserInfo(res.data))
+    .catch((err) => {
+      console.error(err);
+      alert("로그인 실패! ID와 비밀번호를 확인하세요.");
+    })
+    .finally(() => toggleLoading(false));
 };
 
-// 로그아웃 버튼을 클릭하는 경우
+// 로그아웃 버튼 클릭 이벤트
 logoutButton.onclick = () => {
-  logout().then((res) => {
-    console.log(res);
-    renderLoginForm();
-  });
+  toggleLoading(true);
+  logout()
+    .then(() => renderLoginForm())
+    .catch((err) => {
+      console.error(err);
+      alert("로그아웃에 실패했습니다. 다시 시도해 주세요.");
+    })
+    .finally(() => toggleLoading(false));
 };
